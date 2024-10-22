e3032b8b67075893b0c99ec7d67dcb1a26c07538
unknown <idinizramos@gamil.com>
2024-10-22 13:12:33 -0300

    #

--- src/App.java
+++ src/App.java
@@ -2,17 +2,47 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class App {
+
+    public 
     public static void main(String[] args) throws Exception {
+    /*     Scanner scan = new Scanner (System.in);
+        int opcoes = scan.nextInt()
 
-        char matriz[][] = new char[][] { { '1', '2', '3' }, { '4', '5', '6' }, { '7', '8', ' ' } };
+        switch (opcoes) {
+            case 1:
 
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz.length; j++) {
-                if (matriz[0][0] == ' ') {
-                    matriz[2][2]= matriz[2][1];
-                    matriz[2][1] = ' ';
+                break;
+
+            default:
+                break;
+        }*/
+
+        int matriz[][] = new int[][] { { 0, 2, 3 }, { 4, 5, 6 }, { 7, 8, 1 } };
+
+        for (int linha = 0; linha < matriz.length; linha++) {
+            for (int coluna = 0; coluna < matriz.length; coluna++) {
+                if (matriz[linha][coluna] == 0 && linha == 0) {
+                    //if para primeira posição
+                    if (coluna == 0) {
+                        int dir = matriz[linha][coluna + 1];
+                        int baix = matriz[linha + 1][coluna];
+                        int sort = new Random().nextInt(0, 2);
+                        for (int i = 0; i < matriz.length; i++) {
+                            for (int j = 0; j < matriz.length; j++) {
+                                if (sort == 0){
+                                    matriz[linha][coluna] = dir;
+                                    matriz[linha][coluna + 1] = 0;}
+                            if(sort ==1){
+                                matriz[linha][coluna] = baix;
+                                matriz[linha + 1][coluna] =0; 
+                            }
+                        }}
+                        
+                    }
+
                 }
-                System.out.print(matriz[i][j] + " ");
+
+                System.out.print(matriz[linha][coluna] + " ");
 
             }
             System.out.println();
